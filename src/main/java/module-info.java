2aae8c4f0fcda0e3f3a568795b5902733be546fd
Flamyngu <lvl14egiant@gmail.com>
2025-04-30 08:50:34 +0200

    First commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module org.example.spaceinvaders {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens org.example.spaceinvaders to javafx.fxml;
+    exports org.example.spaceinvaders;
+}
